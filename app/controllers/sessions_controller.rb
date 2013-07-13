210c2bd3e6e5a557f83dc28ecd0d837c51a31d59
Lior Bendat <liorbendat@gmail.com>
2013-07-13 13:38:00 -0700

    Working twitter auth

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,7 @@
 class SessionsController < ApplicationController
   def create
-    raise request.env["omniauth.auth"].to_yaml
-    # user = User.from_omniauth(env['omniauth.auth'])
-    # # session[:user_id] = user.id
-    # redirect_to root_url, notice: "Signed in."
+     user = User.from_omniauth(env['omniauth.auth'])
+     session[:user_id] = user.id
+     redirect_to root_url, notice: "Signed in."
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,25 +3,52 @@ class User < ActiveRecord::Base
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :omniauthable
+         :recoverable, :rememberable, :trackable, :validatable, :omniauthable, :omniauth_providers => [:twitter]
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_accessible :email, :username, :password, :password_confirmation, :remember_me
   attr_accessible :name, :twitter, :business_id, :contest_id, :prize_id
   has_and_belongs_to_many :businesses
 
   has_and_belongs_to_many :contests
   has_and_belongs_to_many :prizes
 
-  def self.from_omniauth(auth)
-     where(auth.slice("provider", "uid")).first || create_from_omniauth(auth)
+  # def self.from_omniauth(auth)
+  #    where(auth.slice("provider", "uid")).first || create_from_omniauth(auth)
+  #  end
+
+   def self.from_omniauth(auth)
+     where(auth.slice(:provider, :uid)).first_or_create do |user|
+         user.provider = auth.provider
+         user.uid = auth.uid
+         user.username = auth.info.nickname
+       end
    end
 
-   def self.create_from_omniauth(auth)
-     create! do |user|
-       user.provider = auth["provider"]
-       user.uid = auth["uid"]
-       user.name = auth["info"]["nickname"]
+   def self.new_with_session(params, session)
+     if session["devise.user_attributes"]
+       new(session["devise.user_attributes"], without_protection: true) do |user|
+         user.attributes = params
+         user.valid?
+       end
+     else
+       super
+     end
+   end
+
+   def password_required?
+     super && provider.blank?
+   end
+
+   def email_required?
+     super && provider.blank?
+   end
+
+   def update_with_password(params, *options)
+     if encrypted_password.blank?
+       update_attributes(params, *options)
+     else
+       super
      end
    end
 end
